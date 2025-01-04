93e847ef15f5a4478e5ae27cc6ebb50a2cf27667
Fariha Asif <163842739+Fariha-Asif@users.noreply.github.com>
2025-01-04 11:49:43 +0500

    changes in code

--- src/sanity/lib/image.ts
+++ src/sanity/lib/image.ts
@@ -1,12 +1,10 @@
-import createImageUrlBuilder from '@sanity/image-url'
+import createImageUrlBuilder from '@sanity/image-url';
 import { SanityImageSource } from "@sanity/image-url/lib/types/types";
+import { dataset, projectId } from '../env';
 
-import { dataset, projectId } from '../env'
-import { Image } from 'sanity';
+// Create the image URL builder
+const builder = createImageUrlBuilder({ projectId, dataset });
 
-// https://www.sanity.io/docs/image-url
-const builder = createImageUrlBuilder({ projectId, dataset })
-
-export const urlForImage = (source: Image) => {
-  return builder?.image(source).auto('format').fit('max').url()
-}
+export const urlForImage = (source: SanityImageSource) => {
+  return builder.image(source).auto('format').fit('max').url();
+};
